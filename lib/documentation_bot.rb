9ffdd5a33566d628d72d41dca2c26925b7eee37f
Rob Hesketh <rhesketh.uk@gmail.com>
2016-08-29 21:05:02 +0100

    Two simple unit tests have been added, but they are passing.

--- lib/documentation_bot.rb
+++ lib/documentation_bot.rb
@@ -1,3 +1,7 @@
 class DocumentationBot
-	
+	def initialize(slack_api_token)
+		raise ArgumentError("A Slack API token must be specified as the first parameter") if slack_api_token.empty?
+
+		@slack_api_token = slack_api_token
+	end	
 end
--- spec/lib/documentation_bot_spec.rb
+++ spec/lib/documentation_bot_spec.rb
@@ -1,8 +1,9 @@
+require 'slack-ruby-client'
 require 'spec_helper'
 require File.join($ROOT, 'lib', 'documentation_bot')
 
 describe DocumentationBot do
-  let(:SLACK_API_KEY) { "DUMMY-12345-dummy-67890-DMY" }
+  let(:slack_api_key) { "DUMMY-12345-dummy-67890-DMY" }
 
   describe "#new" do
     it "errors if the Slack API key is passed in as first argument" do
@@ -11,8 +12,8 @@ describe DocumentationBot do
 
     it "does not error if the Slack API key is passed in as first argument" do
       expect{
-      doc_bot = DocumentationBot.new(SLACK_API_KEY)
-      }.not_to raise_error(ArgumentError)
+        doc_bot = DocumentationBot.new(slack_api_key)
+      }.not_to raise_error
     end
   end
 end
