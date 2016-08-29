3e1d0858fd0a5d734628522a311f915f0c95ff7d
Rob Hesketh <rhesketh.uk@gmail.com>
2016-08-29 23:03:43 +0100

    Added tests that check the behaviour of #start!

--- spec/lib/documentation_bot_spec.rb
+++ spec/lib/documentation_bot_spec.rb
@@ -10,8 +10,36 @@ describe DocumentationBot do
 
     it "does not error if the Slack API key is passed in as first argument" do
       expect{
-        doc_bot = DocumentationBot.new(slack_api_key)
+        doc_bot = DocumentationBot.new(slack_api_token)
       }.not_to raise_error
+    end
+  end
+
+  describe '#start!' do
+    describe ':test_authentication option' do
+      it "Should perform an auth test when true" do 
+        slack_client_double = spy("Slack::RealTime::Client")
+        doc_bot = DocumentationBot.new(slack_api_token)
+
+        expect(slack_client_double).to receive(:auth_test)
+        doc_bot.start!(test_authentication: true, slack_client: slack_client_double)
+      end
+
+      it "Should not perform an auth test when false" do
+        slack_client_double = spy("Slack::RealTime::Client")
+        doc_bot = DocumentationBot.new(slack_api_token)
+
+        expect(slack_client_double).not_to receive(:auth_test)
+        doc_bot.start!(test_authentication: false, slack_client: slack_client_double)
+      end
+
+      it "Should not perform an auth test when not present" do 
+        slack_client_double = spy("Slack::RealTime::Client")
+        doc_bot = DocumentationBot.new(slack_api_token)
+
+        expect(slack_client_double).not_to receive(:auth_test)
+        doc_bot.start!(slack_client: slack_client_double)
+      end
     end
   end
 end
