d018d4c8cff91264081c444eb152f9e70a6571c3
Rob Hesketh <rhesketh.uk@gmail.com>
2016-09-06 01:20:43 +0100

    Added rudimentary (and untested) bot response facilities. It will look up a command you give it and apologise if it can't find anything, plus do some basic formatting.

--- lib/documentation_bot.rb
+++ lib/documentation_bot.rb
@@ -13,6 +13,8 @@ class DocumentationBot
 	def start!(options = {})
 		@output = options[:output]			|| STDOUT
 		@client = options[:slack_client] 	|| Slack::RealTime::Client.new
+		@ri_lookup = options[:ri_lookup]	|| RiLookup.new
+		@slack_parser = options[:parser]	|| MarkdownToSlack.new
 		@client.auth_test if options[:test_authentication]
 
 		@client.on :hello do
@@ -29,10 +31,54 @@ class DocumentationBot
 			@output.puts "==> I've left channel ##{@client.channels[e.channel]['name']}"
 		end
 
+		@client.on :message do |message_data|
+			next unless message_is_addressed_to_the_bot? message_data
+			next if message_originates_from_the_bot? message_data
+
+			command = get_command_from_incoming_message message_data["text"]
+
+			raw_output = @ri_lookup.find(command)
+			clean_output = @slack_parser.convert raw_output
+			clean_output = "Sorry, couldn't find any information about '#{command}'." if clean_output.empty?
+
+			@client.message channel: message_data.channel, text: clean_output
+		end
+
 		@client.start!
 	end
 
 	private 
+	def get_command_from_incoming_message(message)
+		output = message.gsub(/^\<\@#{@client.self.id}\> /, "")	# Strip our userid if it's in there
+		output = output.gsub(/^#{@client.self.name} /, "") 		# Strip our username if it's in there
+
+		# Whatever else remains is our command
+		return output
+	end
+
+	def message_originates_from_the_bot?(message_data)
+		return message_data["user"] == @client.self.id
+	end
+
+	def message_is_addressed_to_the_bot?(message_data)
+		return true if message_is_direct_message message_data["channel"]
+		return true if user_id_was_mentioned? message_data["text"]
+		return true if username_was_mentioned? message_data["text"]
+
+		return false
+	end
+
+	def user_id_was_mentioned?(message)
+		return message.scan(/^\<\@#{@client.self.id}\>/).count > 0
+	end
+
+	def username_was_mentioned?(message)
+		return message =~ /^#{@client.self.name}/
+	end
+
+	def message_is_direct_message(channel)
+		return channel[0] == "D"
+	end
 
 	def channels_i_am_a_member_of
 		@client.channels.map{|a| a[1]}.select{|c| c["is_member"]}.map{|c| c["name"]}
