5eb2b50e8feefd26f5c88a5a5103f31735c7c1da
Rob Hesketh <rhesketh.uk@gmail.com>
2016-09-07 16:28:32 +0100

    Strip characters that aren't useful for conventional ri lookups. This helps prevent issues with security.

--- lib/ri_lookup.rb
+++ lib/ri_lookup.rb
@@ -1,8 +1,15 @@
 class RiLookup
 	def find(command)
+		command = strip_weird_characters(command)
 		lookup_result = %x[ri #{command} --format=markdown]
 
 		return nil if lookup_result.to_s.empty?
 		return lookup_result
+	end
+
+	private
+
+	def strip_weird_characters(input)
+		return input.gsub(/[^A-Za-z\d:_.]/, "")
 	end
 end
